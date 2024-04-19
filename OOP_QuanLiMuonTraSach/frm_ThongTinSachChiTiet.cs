6cd7b2de93095f3985376b518f8edd0160ed2b44
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-04-19 11:50:54 +0700

    complete code

--- OOP_QuanLiMuonTraSach/frm_ThongTinSachChiTiet.cs
+++ OOP_QuanLiMuonTraSach/frm_ThongTinSachChiTiet.cs
@@ -61,28 +61,43 @@ namespace OOP_QuanLiMuonTraSach
         {
             List<QuanLiMuonTraSach> userList = ThuVien.GetInstance().Employee.ListQuanLiMuonTraSach.UserListQuanLiMuonTraSach(idNguoiDung).QuanLiMuonTraSaches;
             int countSoTienPhat = 0;
-            foreach(QuanLiMuonTraSach item in userList)
+            Book bookFind = ThuVien.GetInstance().Employee.FindBook(idSach);
+            bool isYeuCauMuonSach = false;
+            foreach (QuanLiMuonTraSach item in userList)
             {
                 if (item.SoTienPhat > 0)
                 {
                     countSoTienPhat++;
                 }
+                if (item.TinhTrang == "Yêu cầu" && item.TenSach == bookFind.TenSach)
+                {
+                    isYeuCauMuonSach = true;
+                }
+                else isYeuCauMuonSach = false;
             }
             if (countSoTienPhat >= 3)
             {
                 MessageBox.Show("Bạn đã trả sách quá hạn nhiều lần \nVui lòng thanh toán để tiếp tục mượn sách", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-            Book bookFind = ThuVien.GetInstance().Employee.FindBook(idSach);
-            if (bookFind == null || bookFind.SoLuong == 0)
+           
+            if (bookFind == null || bookFind.SoLuong <= 0)
             {
                 MessageBox.Show("Số lượng sách đã hết, vui lòng mượn sách khác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
-            QuanLiMuonTraSach qlmts = new QuanLiMuonTraSach(bookFind, student);
-            ThuVien.GetInstance().Employee.AddMuonTraSach(qlmts);
-            ThuVienController.Serialize<ListQuanLiMuonTraSach>(FilePath.QuanLiMuonTraSach, ThuVien.GetInstance().Employee.ListQuanLiMuonTraSach);
-            MessageBox.Show("Yêu cầu mượn sách thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (isYeuCauMuonSach == true)
+            {
+                MessageBox.Show($"Bạn đã tạo yêu cầu mượn sách cho sách {bookFind.TenSach} \nVui lòng liên hệ nhân viên để xác nhận mượn sách","Thông báo",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            if (isYeuCauMuonSach == false)
+            {
+                QuanLiMuonTraSach qlmts = new QuanLiMuonTraSach(bookFind, student);
+                ThuVien.GetInstance().Employee.AddMuonTraSach(qlmts);
+                ThuVienController.Serialize<ListQuanLiMuonTraSach>(FilePath.QuanLiMuonTraSach, ThuVien.GetInstance().Employee.ListQuanLiMuonTraSach);
+                MessageBox.Show("Yêu cầu mượn sách thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
         #endregion
     }
